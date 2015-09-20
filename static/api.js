fc5dba9c45441593a22d5e7b59d32bba54f6bb9b
Sebastiano Gottardo <dextorer@gmail.com>
2015-09-20 19:21:04 +0200

    added missing api.js

---
+++ static/api.js
@@ -0,0 +1,67 @@
+function request(verb, path, onsuccess, onfail) {
+   var xhr = new XMLHttpRequest();
+   xhr.open(verb, path, true);
+   xhr.onload = function (e) {
+      if (xhr.readyState === 4) {
+         if (xhr.status === 200) {
+            onsuccess(xhr.response)
+         } else {
+            onfail(xhr.statusText);
+         }
+      }
+   };
+   xhr.onerror = function (e) {
+      onfail(xhr.statusText);
+   };
+   xhr.send(null);
+}
+
+
+function submitLibraryRequest(libraryName) {
+   request(
+         "POST",  
+         "/api/request/" + libraryName, 
+         function(response) {
+            obj = JSON.parse(response);
+            console.log("Successfully enqeueud job for " + obj["lib_name"]);
+            poll(libraryName);
+         },
+         function(errorText) {
+            console.error(errorText);
+         }
+         );
+}
+
+
+function poll(libraryName) {
+   request("GET", "/api/stats/" + libraryName,
+         function(response) {
+            obj = JSON.parse(response);
+            if(obj["status"] == "done") {
+               console.log("Done");
+               console.log(obj)
+               document.getElementById('output').value = JSON.stringify(obj)
+            }
+            else {
+               setTimeout(function() {
+                  poll(libraryName);
+               }, 4000);
+            }
+         },
+         function(errorText) {
+            console.error(errorText);
+         });
+}
+
+
+function fetchTopLibraries() {
+   request("GET", "/api/top/",
+      function(response) {
+         console.log("response" + response)
+         obj = JSON.parse(response);
+         console.log(obj)
+      },
+      function(errorText) {
+         console.error(errorText)
+      });
+}
