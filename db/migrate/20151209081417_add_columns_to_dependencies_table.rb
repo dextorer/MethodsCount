663a5e1a0a5e0715b9a2b023d856eafce56ce04c
Nicola Miotto <sirnicolaz@gmail.com>
2015-12-26 11:08:19 +0100

    fix tabs and require data migration

--- db/migrate/20151209081417_add_columns_to_dependencies_table.rb
+++ db/migrate/20151209081417_add_columns_to_dependencies_table.rb
@@ -1,15 +1,15 @@
 class AddColumnsToDependenciesTable < ActiveRecord::Migration
   def change
-  	add_column :dependencies, :library_name, :string
-  	add_column :dependencies, :dependency_name, :string
-  	add_index :dependencies, :library_name
+    add_column :dependencies, :library_name, :string
+    add_column :dependencies, :dependency_name, :string
+    add_index :dependencies, :library_name
   end
 
   def data
-  	Dependencies.find_each do |dep|
-  		dep.library_name = Libraries.find(dep.library_id).fqn
-  		dep.dependency_name = Libraries.find(dep.dependency_id).fqn
+    Dependencies.find_each do |dep|
+      dep.library_name = Libraries.find(dep.library_id).fqn
+      dep.dependency_name = Libraries.find(dep.dependency_id).fqn
       dep.save!
-  	end
+    end
   end
 end
