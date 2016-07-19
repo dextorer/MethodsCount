0dbc79d6297e4b071e49dcd95e5558304d808d67
Nicola Miotto <sirnicolaz@gmail.com>
2016-07-19 16:55:18 +0200

    fix sdk update script

--- ops/01_command_step.sh
+++ ops/01_command_step.sh
@@ -6,14 +6,17 @@ if [ ! -f $TMP_DIR/command.checkpoint ]; then
 	INSTANCE_ID=$(echo $RESPONSE | sed -e 's/.*InstanceId": "\(.*\)", "ImageId.*/\1/g')
 	echo $INSTANCE_ID > $TMP_DIR/instance_id
 	echo "Waiting for instance ${INSTANCE_ID} to be available..."
-	until RUNNING=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | grep running)
+	sleep 10
+	until RUNNING=$(aws ec2 describe-instance-status --instance-ids $INSTANCE_ID | grep passed)
 	do
 		sleep 1
 	done
-	sleep 5
+	sleep 10
 	echo "Instance is running"
 	echo "Updating sdk on instance id "$INSTANCE_ID
-	RESPONSE=$(aws ssm send-command --document-name AWS-RunShellScript --parameters '{"commands":["echo y | android-sdk-linux/tools/android update sdk --no-ui"],"workingDirectory":["/var/android"],"executionTimeout":["3600"]}' --instance-ids $INSTANCE_ID --output-s3-bucket-name ami-provisioning-log)
+	COMMAND="aws ssm send-command --document-name AWS-RunShellScript --parameters '{\"commands\":[\"echo y | android-sdk-linux/tools/android update sdk --no-ui\"],\"workingDirectory\":[\"/var/android\"],\"executionTimeout\":[\"3600\"]}' --instance-ids $INSTANCE_ID --output-s3-bucket-name ami-provisioning-log"
+	echo $COMMAND
+	RESPONSE=$(eval $COMMAND)
 	echo $RESPONSE > $TMP_DIR/command_response.json
 	touch $TMP_DIR/command.checkpoint
 fi
