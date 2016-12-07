e94b2d0670361fe1d6b3d71034b0931c81cbc06d
Nicola Miotto <sirnicolaz@gmail.com>
2016-12-07 10:39:17 +0100

    fix update sdk script

--- ops/02_wait_command.sh
+++ ops/02_wait_command.sh
@@ -2,7 +2,7 @@
 TMP_DIR=tmp
 
 RESPONSE=$(cat $TMP_DIR/command_response.json)
-COMMAND_ID=$(echo $RESPONSE | sed -e 's/.*CommandId": "\(.*\)", "Requ.*/\1/g')
+COMMAND_ID=$(echo $RESPONSE | sed -e 's/.*CommandId": "\(.*\)", "I.*/\1/g')
 echo "Waiting for update command id ${COMMAND_ID} to finish ."
 until DONE=$(aws s3 ls s3://ami-provisioning-log/$COMMAND_ID)
 do
